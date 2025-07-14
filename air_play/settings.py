742b13bd290ca6123ebfe1cc796c9510cc4c7cf8
Titus <handsomekhalo@gmail.com>
2025-07-14 10:01:40 +0200

    user moidels db setup

--- air_play/settings.py
+++ air_play/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
      'rest_framework.authtoken',
      'rest_framework_simplejwt',
     'corsheaders',
+    'system_management',
 
 ]
 
@@ -157,6 +158,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'system_management.User'
+
 
 
 
